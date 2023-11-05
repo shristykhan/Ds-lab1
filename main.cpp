f989f4de8ffb2f2ce66dd748370b576ce3dd4e9c
shristykhan <149964427+shristykhan@users.noreply.github.com>
2023-11-06 01:05:36 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,131 @@
+
+#include <iostream>
+
+using namespace std;
+
+
+struct student {
+    int id;
+    int semester;
+    int credit;
+    double cg;
+};
+int main()
+{
+
+student stu1[1];
+stu1[1].id=22-46322-1;
+stu1[1].semester=6;
+stu1[1].credit=61;
+stu1[1].cg=3.97;
+
+cout<<stu1.id<<""<<endl;
+cout<<  <<endl;
+cout<<  <<endl;
+cout<<   <<endl;
+
+stu[2].id=21-463512-3;
+stu[2].semester=10;
+stu[2].credit=80;
+stu[2].cg=3.75;
+
+cout<<stu1.id<<""<<endl;
+cout<<  <<endl;
+cout<<  <<endl;
+cout<<   <<endl;
+
+
+stu[3].id=23-46291-1;
+stu[3].semester=2;
+stu[3].credit=15;
+stu[3].cg=3.20;
+
+cout<<stu1.id<<""<<endl;
+cout<<  <<endl;
+cout<<  <<endl;
+cout<<   <<endl;
+
+stu[4].id=22-47851-2;
+stu[4].semester=5;
+stu[4].credit=57;
+stu[4].cg=4;
+
+cout<<stu1.id<<""<<endl;
+cout<<  <<endl;
+cout<<  <<endl;
+cout<<   <<endl;
+
+stu[5].id=20-46351-2;
+stu[5].semester=9;
+stu[5].credit=80;
+stu[5].cg=3.00;
+
+cout<<stu1.id<<""<<endl;
+cout<<  <<endl;
+cout<<  <<endl;
+cout<<   <<endl;
+
+stu[6].id=21-46356-1;
+stu[6].semester=8;
+stu[6].credit=80;
+stu[6].cg=3.75;
+
+cout<<stu1.id<<""<<endl;
+cout<<  <<endl;
+cout<<  <<endl;
+cout<<   <<endl;
+
+stu[7].id=22-46351-3;
+stu[7].semester=4;
+stu[7].credit=65;
+stu[7].cg=2.91;
+
+cout<<stu1.id<<""<<endl;
+cout<<  <<endl;
+cout<<  <<endl;
+cout<<   <<endl;
+
+stu[8].id=23-46366-1;
+stu[8].semester=2;
+stu[8].credit=15;
+stu[8].cg=3.20;
+
+cout<<stu1.id<<""<<endl;
+cout<<  <<endl;
+cout<<  <<endl;
+cout<<   <<endl;
+
+stu[9].id=20-46281-2;
+stu[9].semester=10;
+stu[9].credit=80;
+stu[9].cg=3.60;
+
+cout<<stu1.id<<""<<endl;
+cout<<  <<endl;
+cout<<  <<endl;
+cout<<   <<endl;
+
+stu[10].id=20-46351-1;
+stu[10].semester=10;
+stu[10].credit=95;
+stu[10].cg=3.50;
+
+cout<<stu1.id<<""<<endl;
+cout<<  <<endl;
+cout<<  <<endl;
+cout<<   <<endl;
+
+
+cout << "\nScholarship Students (CGPA >= 3.75):" << endl;
+    for (int i = 1; i < 11; i++) {
+        if (st[i].cgpa >= 3.75) {
+            cout << "Student ID: " << st[i].id << endl;
+        }
+    }
+
+
+    return 0;
+}
+
+
+
